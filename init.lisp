35da63924147e8500a3d62ca6c45b8746d13476a
helaveesa <helaveesa.nolder@gmail.com>
2012-11-08 02:43:21 +0400

    add img-dezine

--- init.lisp
+++ init.lisp
@@ -19,7 +19,7 @@
     (:link "/contacts" :title "Контакты")))
 
 ;; start
-(restas:start '#:strahbroker :port 110)
+(restas:start '#:strahbroker :port 2001)
 (restas:debug-mode-on)
 ;; (restas:debug-mode-off)
 (setf hunchentoot:*catch-errors-p* t)
